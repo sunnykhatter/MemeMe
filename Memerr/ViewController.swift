00d790318405c74336609673f14ea4bd844d4e8f
Lakshay Khatter <lakshaykhatter@gmail.com>
2016-07-19 11:57:41 -0400

    Initial Commit

--- Memerr/ViewController.swift
+++ Memerr/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let imagePicker = UIImagePickerController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +22,9 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    
+    
 
 
 }
