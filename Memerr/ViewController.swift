0b913ff8495822fd3612420a5cf1b9529ecbcae0
Lakshay Khatter <lakshaykhatter@gmail.com>
2016-08-01 18:01:31 -0400

    Implemented Photopicker and text labels

--- Memerr/ViewController.swift
+++ Memerr/ViewController.swift
@@ -8,14 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
 
+    @IBOutlet weak var topTextField: UITextField!
+
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    
     let imagePicker = UIImagePickerController()
     
+    @IBOutlet weak var imagePickerView: UIImageView!
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let memeTextAttributes = [
+            NSStrokeColorAttributeName : UIColor.blackColor(),
+            NSForegroundColorAttributeName : UIColor.whiteColor(),
+            NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 25)!,
+            NSStrokeWidthAttributeName : -2.0,
+            
+        ]
+        
+        topTextField.defaultTextAttributes = memeTextAttributes
+        topTextField.adjustsFontSizeToFitWidth = true
+        topTextField.textAlignment = NSTextAlignment.Center
+        
+        
+        
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.adjustsFontSizeToFitWidth = true
+        bottomTextField.textAlignment = NSTextAlignment.Center
+    
+        topTextField.hidden = true
+        bottomTextField.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,9 +51,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    
-    
 
+    @IBAction func pickAnImage(sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        imagePickerView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        dismissViewControllerAnimated(true, completion: nil)
+        topTextField.hidden = false
+        bottomTextField.hidden = false
+    }
 
 }
 
